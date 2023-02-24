f65cd2c0796baeeea54afeae077a38284207d20a
Louis-duc <ducrocq.louis16@gmail.com>
2023-02-24 18:27:31 +0100

    complete register and login activity

--- app/src/main/java/com/example/projetvinted/Register.java
+++ app/src/main/java/com/example/projetvinted/Register.java
@@ -3,6 +3,7 @@ package com.example.projetvinted;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -28,6 +29,17 @@ public class Register extends AppCompatActivity {
     TextView textView;
 
     @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
@@ -37,6 +49,14 @@ public class Register extends AppCompatActivity {
         buttonReg = findViewById(R.id.register_btn);
         progressBar = findViewById(R.id.progressBar);
         textView = findViewById(R.id.loginNow);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +84,9 @@ public class Register extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     Toast.makeText(Register.this, "Account created.",
                                             Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                                    startActivity(intent);
+                                    finish();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Toast.makeText(Register.this, "Account creation failed.",
