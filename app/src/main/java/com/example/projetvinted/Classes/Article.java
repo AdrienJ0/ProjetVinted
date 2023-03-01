974ff4fe111a2d90ac638fc4ae2f8fc10df127c2
AdrienJ0 <adrientchuemtchuente@gmail.com>
2023-03-01 08:59:40 +0100

    Update

---
+++ app/src/main/java/com/example/projetvinted/Classes/Article.java
@@ -0,0 +1,47 @@
+package com.example.projetvinted.Classes;
+/***
+ * Class which contain all the data about an article
+ *
+ * */
+public class Article {
+
+    private String articleName;
+    private String articlePrice;
+    private String articleImage;
+    //private ClassSeller articleSeller;
+
+    public Article(){
+
+    }
+
+    public Article(String articleName, String articlePrice, String articleImage) {
+        this.articleName = articleName;
+        this.articlePrice = articlePrice;
+        this.articleImage = articleImage;
+    }
+
+    public String getArticleImage() {
+        return articleImage;
+    }
+
+    public void setArticleImage(String articleImage) {
+        this.articleImage = articleImage;
+    }
+
+    public String getArticleName() {
+        return articleName;
+    }
+
+    public void setArticleName(String articleName) {
+        this.articleName = articleName;
+    }
+
+    public String getArticlePrice() {
+        return articlePrice;
+    }
+
+    public void setArticlePrice(String articlePrice) {
+        this.articlePrice = articlePrice;
+    }
+
+}
