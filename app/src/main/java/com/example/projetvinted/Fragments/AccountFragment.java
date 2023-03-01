7ae9a83d4202d759f1f5ccc28bf2d11cf076676a
JeanMroziewicz <jean.mroziewicz@isen.yncrea.fr>
2023-03-01 12:09:24 +0100

    update

--- app/src/main/java/com/example/projetvinted/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/projetvinted/Fragments/AccountFragment.java
@@ -1,24 +1,48 @@
 package com.example.projetvinted.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.projetvinted.Login;
 import com.example.projetvinted.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 /***
  * Class which is associated to the layout fragment_account
  */
 public class AccountFragment extends Fragment {
 
+    Button button;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view =inflater.inflate(R.layout.fragment_account, container, false);
+
+        button = view.findViewById(R.id.logout);
+
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), Login.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+    }
+
 }
